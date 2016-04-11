6ed1f6e2a25878eb7225c5309dcac1e45780b6aa
c86jeff <zhangjiexin0101@gmail.com>
2016-04-11 17:24:22 +0800

    update workerman dir

--- service.php
+++ service.php
@@ -1,7 +1,7 @@
 <?php
 use Workerman\Worker;
 use \Workerman\Lib\Timer;
-require_once '../workerman/Autoloader.php';
+require_once '../Workerman/Autoloader.php';
 require_once 'create.php';
 // Create a Websocket server
 $ws_worker = new Worker("websocket://121.42.144.9:4000");
