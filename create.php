e06e85f73a6ce21b3f683dae6d1f8112a66c603a
reasno <guxi99@gmail.com>
2016-05-09 17:40:27 +0800

    add siteprefix check

--- create.php
+++ create.php
@@ -3,6 +3,8 @@
 require_once('/var/www/html/SiteMaintenance/WikiSite/WikiSite.php');
 require_once('/var/www/html/SiteMaintenance/WikiSite/Invitation.php');
 require_once('/var/www/html/SiteMaintenance/WikiSite/ErrorMessage.php');
+require_once('/var/www/html/SiteMaintenance/WikiSite/DBUtility.php');
+
 function receive_message($conn, $data){
 	if(!$conn || !$data) return;
 	$in = json_decode($data);
@@ -19,6 +21,10 @@ function receive_message($conn, $data){
 			if(Invitation::checkInvitation($params->invitationCode) == ErrorMessage::INV_NOT_FOUND){
 				$conn->send(generate_send_message("fail", "invitation code is not valid", "info"));	
 				return;
+			}
+			if(checkSitePrefix($params->domainPrefix) != 0){
+				$conn->send(generate_send_message("fail", "siteprefix is not valid", "info"));
+                                return;
 			}
 			$conn->send(generate_send_message("success", "Start building new wikisite: ".$params->domainPrefix, "info"));
 			$wikisite = new WikiSite($params->domainPrefix, $params->domainName, $params->domainType, $params->domainDescription, 
@@ -46,6 +52,23 @@ function generate_send_message($status, $message, $action){
 	];
 
 	return json_encode($o);
+}
+
+function checkSitePrefix($prefix){
+    $reg = "/^[A-Za-z0-9][A-Za-z0-9-]*$/i";
+    if ( empty($prefix) ) {
+	return -1;
+    }elseif ( strlen($prefix) < 3 || strlen($prefix) > 20 ) {
+	return -1;
+    }elseif( preg_match($reg, $prefix) !== 1 ){
+	return -1;
+    }elseif (DBUtility::domainExists( $prefix) ) {
+	return -1;
+    }elseif ( strpos ($prefix, 'fuck') !== false || strpos ($prefix, 'sex') !== false || strpos ($prefix, 'porn') !== false) {
+	return -1;
+    }else{
+	return 0;
+    }
 }
 
 ?>
